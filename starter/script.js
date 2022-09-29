c48e3869d068db3c2080e4734ecc2f5fc062508b
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-09-29 14:41:39 -0600

    maptys leaflets latitude longitude

---
+++ starter/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+// Using the Geolocation API
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},-${longitude}`);
+      //set latitude and longitude to coords
+      const coords = [latitude, longitude];
+      //coords and zoom level high number closer lower farther out
+      const map = L.map('map').setView(coords, 13);
+      //fr/hot was org
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+      // set the marker icon to the coords of latitude and longitude using current location
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );

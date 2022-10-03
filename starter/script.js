3efedd47253f904c44eff63b9563f5ba7d3be435
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-10-03 13:02:47 -0600

    update evemt listener change

--- starter/script.js
+++ starter/script.js
@@ -85,4 +85,7 @@ form.addEventListener('submit', function (e) {
     .setPopupContent('Workout')
     .openPopup();
 });
-inputType.addEventListener('change', function () {});
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
